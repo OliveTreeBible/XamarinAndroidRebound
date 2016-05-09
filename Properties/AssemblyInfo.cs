4ae07366f4d9c5947448f8eabca7ceb9e31f3e83
Adam Hewitt <adamhewitt@olivetree.com>
2016-05-09 12:29:12 -0700

    setup nuget

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,9 @@ using Android.App;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Xamarin.Android.Rebound")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(@"Xamarin bindings for the Facebook Rebound library: https://github.com/facebook/rebound
+
+This project does not offer support for Rebound, but will match version numbering.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Xamarin.Android.Rebound")]
@@ -16,15 +18,5 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.8.*")]
+[assembly: AssemblyFileVersion("0.3.8")]
